3ad35c8a937b05f6e63def8217f962d48796e7b7
Ryss-D <70545185+Ryss-D@users.noreply.github.com>
2022-09-02 16:06:16 -0500

    feat: updated local db interaction

--- lib/data/data_providers/posts_db.dart
+++ lib/data/data_providers/posts_db.dart
@@ -8,12 +8,18 @@ class DBHelper {
     //getDatabaesPath method search for the location dependly of the operaing system
     final dbPath = await sql.getDatabasesPath();
     return sql.openDatabase(
-      path.join(dbPath, 'places.db'),
+      path.join(dbPath, 'posts.db'),
       //this is the method who will be executed if we try to open the
       //db file and didnt find anythings
-      onCreate: (db, version) {
-        return db.execute(
-          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, loc_lat REAL, loc_lng REAL, address TEXT)',
+      onCreate: (db, version) async {
+        await db.execute(
+            'CREATE TABLE posts(post_id INTEGER PRIMARY KEY, user_id INTEGER, title TEXT, body TEXT, is_favorite INTEGER)');
+
+        await db.execute(
+            'CREATE TABLE authors(user_id INTEGER PRIMARY KEY, name TEXT, username TEXT,  email TEXT, address_street TEXT, address_suite TEXT, address_city TEXT, address_zipcode TEXT, geo_lat TEXT, geo_lng TEXT, phone TEXT, website TEXT, company_name TEXT, company_catchphrase TEXT, company_bs TEXT)');
+
+        await db.execute(
+          'CREATE TABLE comments(comment_id INTEGER PRIMARY KEY, post_id INTEGER, name TEXT, email TEXT, body TEXT)',
         );
       },
       version: 1,
